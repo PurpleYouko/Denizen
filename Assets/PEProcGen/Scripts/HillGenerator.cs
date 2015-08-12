701fc9cb3f9ffe8803dd1753d22042c006006c67
purpleyouko <purpleyouko@gmail.com>
2015-08-12 12:09:42 -0500

    Reworked the code so that we can set a groundDepth and hillDepth variable from unity control panel. Also removed yOffset

--- Assets/PEProcGen/Scripts/HillGenerator.cs
+++ Assets/PEProcGen/Scripts/HillGenerator.cs
@@ -6,7 +6,8 @@ public class HillGenerator : BaseGenerator
 	public float heightScale = 1.0F;
 	public float xScale = 1.0F;
 	public float yNoise = 0f;
-	public float yOffset = 0f;
+	public float groundDepth = 10.0f;
+	public float hillDepth = 20.0f;
 
 	public AnimationCurve contourCurve = AnimationCurve.Linear (0f, 0f, 1f, 1f);
 
@@ -17,11 +18,13 @@ public class HillGenerator : BaseGenerator
 		for(int x = 0;x < MapWidth;x++)
 		{
 			float xf = (float)x / (float)(MapWidth-1);
+			//height will evaluate to a value ~ between 0 and 1
 			float height = contourCurve.Evaluate(xf) * heightScale * Mathf.PerlinNoise(xf * xScale, yNoise);
+			height *= hillDepth;
+			height += groundDepth;
 			for(int y = 0;y < MapHeight;y++)
 			{
-				float yf = (float)(y - yOffset) / (float)(MapHeight-1);
-				if(yf <= height)
+				if(y <= height)
 				{
 					CurrentMap[x,y] = 1;
 				}
