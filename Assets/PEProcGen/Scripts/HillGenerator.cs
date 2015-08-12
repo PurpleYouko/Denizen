b7062d41b9ffa30879e8d13a237526a8c0578799
purpleyouko <purpleyouko@gmail.com>
2015-08-12 11:04:48 -0500

    removed comments

--- Assets/PEProcGen/Scripts/HillGenerator.cs
+++ Assets/PEProcGen/Scripts/HillGenerator.cs
@@ -13,13 +13,10 @@ public class HillGenerator : BaseGenerator
 
 	public void GenerateMap()
 	{
-		//PY new method. One loop for all plus it kicks out once the top of the ground is reached so even faster (in theory)
-		//Clear it first. this is temporary and won't ever be called once the game is finished.
 		ClearMap();
-		//Had to reverse the nesting order
 		for(int x = 0;x < MapWidth;x++)
 		{
-			float xf = (float)x / (float)(MapWidth-1); // moved these calcs out of the inner loop since they only evaluate with x and not y
+			float xf = (float)x / (float)(MapWidth-1);
 			float height = contourCurve.Evaluate(xf) * heightScale * Mathf.PerlinNoise(xf * xScale, yNoise);
 			for(int y = 0;y < MapHeight;y++)
 			{
@@ -30,7 +27,7 @@ public class HillGenerator : BaseGenerator
 				}
 				else
 				{
-					y = MapHeight; //kicks out to the next x as we are already in the sky
+					y = MapHeight; 
 				}
 			}
 		}
@@ -44,14 +41,6 @@ public class HillGenerator : BaseGenerator
 
 	public void Generate () 
 	{
-		//PY This is pointless. We can do it all in one step in about a third the time
-		/*
-		for(int y=0;y<MapHeight;y++){
-			for(int x=0;x<MapWidth;x++){
-				CurrentMap[x,y] = 1;
-			}
-		}
-		*/
 		GenerateMap();
 		if(renderImmediate) Render ();
 	}
